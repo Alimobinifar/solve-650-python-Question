916963017c318e8f66ce4b9da3d552b1887b9e8c
alimobinifar <mobinifarali62@gmail.com>
2021-11-17 11:42:47 +0330

    11:42

---
+++ 650Pproblem/session1_P1.py
@@ -0,0 +1,7 @@
+base =float(input('Please enter length of base :')) #get base
+
+height=float(input('Please Enter measurmnet of height :')) # get height 
+
+result=base*height
+
+print('area is : {}'.format(result))
