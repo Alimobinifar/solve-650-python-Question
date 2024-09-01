6032c3db02d817241bacc7c73bcd137b9db5fdc4
AliMobinifar <88401300+Alimobinifar@users.noreply.github.com>
2024-09-01 16:13:09 +0330

    Delete 650Pproblem directory

--- 650Pproblem/session1_P1.py
+++
@@ -1,7 +0,0 @@
-base =float(input('Please enter length of base :')) #get base
-
-height=float(input('Please Enter measurmnet of height :')) # get height 
-
-result=base*height
-
-print('area is : {}'.format(result))
